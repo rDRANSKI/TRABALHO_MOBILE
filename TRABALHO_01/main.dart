69f327251b548a58703bf51ffe8bc8223c82d2a8
rDRANSKI <ronaldodranskidranski988@gmail.com>
2026-04-01 20:18:59 -0300

    Create main.dart

---
+++ TRABALHO_01/main.dart
@@ -0,0 +1,7 @@
+/// Trabalho 1 - Programação para Dispositivos Móveis
+/// Aluno: [Seu Nome Completo Aqui]
+
+void main() {
+  // A função print exibe a mensagem no terminal
+  print('Hello World');
+}
